0611aa4e55125b81dd727142eb7889ecae33074d
Sebastian Fišer <119879744+SebastianFiser@users.noreply.github.com>
2026-04-29 10:50:19 +0000

    brightness imput clearing for device.py and error fixing

--- setup.sh
+++ setup.sh
@@ -5,13 +5,16 @@ read -p "hiveMQ port: " HIVEMQ_PORT
 read -p "hiveMQ Websocket port: " HIVEMQ_WEBSOCKET_PORT
 read -p "hiveMQ username: " HIVEMQ_USERNAME
 read -p "hiveMQ password: " HIVEMQ_PASSWORD
+read -p "Hivemq ws URL: " HIVEMQ_WS_URL
 echo 
 
 cat > .env << EOF
 HIVEMQ_HOST=$HIVEMQ_HOST
 HIVEMQ_PORT=$HIVEMQ_PORT
+HIVEMQ_WEBSOCKET_PORT=$HIVEMQ_WEBSOCKET_PORT
 HIVEMQ_USERNAME=$HIVEMQ_USERNAME
 HIVEMQ_PASSWORD=$HIVEMQ_PASSWORD
+HIVEMQ_WS_URL=$HIVEMQ_WS_URL
 EOF
 
-echo".env file created with the provided HiveMQ cluster details"
+echo ".env file created with the provided HiveMQ cluster details"
